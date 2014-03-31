4ff059ed7c80053c6585c261173c189e14c4eb76
jtipton25 <jtipton25@gmail.com>
2014-03-31 15:36:51 -0600

    updated wishart code

---
+++ nonSpatialBarcastWishart/mcmc.nonSpatialBarcastWishart.R
@@ -0,0 +1,278 @@
+##
+## Model for analysis of tree ring chronologies at one spatial location ignoring different species
+##
+
+## Next: Remove Sigma, Sigma.inv, Sigma.epsilon and Sigma.epsilon.inv
+##
+##
+## Note that in the code X represents the field T in the Barcast Model
+mcmc <- function(WI, WP, n.mcmc, mu.0.tilde, sigma.squared.0.tilde, alpha.alpha, beta.alpha, mu.0, sigma.squared.0, alpha.sigma.squared, beta.sigma.squared, alpha.phi, beta.phi, alpha.I, beta.I, alpha.P, beta.P, mu.beta.0, sigma.squared.beta.0, mu.beta.1, sigma.squared.beta.1, nu.wish){
+  
+  ##
+  ## libraries and subroutines
+  ##
+  
+  library(truncnorm)
+  
+  make.HIt <- function(i){
+    if(NIt[i] == 0){
+      return(NULL)
+    } else {
+      return(c(1, rep(0, n - 1)))
+    }
+  }
+  
+  make.HPt <- function(i){
+    tmp <- matrix(0, nrow = NPt[i], ncol = n)
+    for(j in 1:NPt[i]){
+      tmp[j, 1 + which(HP[i, ] == 1)[j]] <- 1
+    }
+    return(tmp)
+  }
+  
+  make.Ht <- function(i, beta.1){
+    rbind(HIt[[i]], (HPt[[i]] * beta.1))
+  }
+  
+  make.Wt <- function(i){
+    as.vector(W[i, ][H[i,]])
+  }
+  
+  make.WPt <- function(i){
+    as.vector(WP[i, ][HP.idx[i, ]])
+  }
+  
+  make.WIt <- function(i){
+    if(is.na(WI[i])){
+      NULL
+    } else {
+      as.vector(WI[i][HI.idx[i]])
+    }
+  }
+
+  make.mh <- function(i, Z, mu, alpha, Sigma.epsilon, Sigma.epsilon.inv){
+    - 1 / 2 * determinant(Sigma.epsilon, logarithm = TRUE)$modulus[1] - 1 / 2 * t(Z[i + 1, ] - alpha * Z[i, ] - (1 - alpha) * mu) %*% Sigma.epsilon.inv %*% (Z[i + 1, ] - alpha * Z[i, ] - (1 - alpha) * mu)
+  }
+  
+  ##
+  ## initialize variables
+  ##
+  
+  W <- as.matrix(cbind(WI, WP))
+  WP <- as.matrix(WP)
+  t <- dim(W)[1]
+  n <- dim(W)[2]
+  X <- matrix(0, nrow = t + 1, ncol = n)
+  NI <- 1
+  NP <- dim(WP)[2]
+  NIt <- vector(length = t)
+  NPt <- vector(length = t)
+  Nt <- vector(length = t)
+  for(i in 1:t){
+    NIt[i] <- sum(!is.na(WI[i]))
+    NPt[i] <- sum(!is.na(WP[i, ]))
+    Nt[i] <- NIt[i] + NPt[i]
+  }
+  MI <- sum(NIt)  
+  MP <- sum(NPt)
+  
+  HI.idx <- !is.na(WI)
+  HI <- ifelse(HI.idx, 1, 0)
+  HP.idx <- !is.na(WP)
+  HP <- ifelse(HP.idx, 1, 0)
+  H <- !is.na(W)
+  
+  Wt <- lapply(1:t, make.Wt)
+  WPt <- lapply(1:t, make.WPt)
+  WIt <- lapply(1:t, make.WIt) 
+  HIt <- lapply(1:t, make.HIt)
+  HPt <- lapply(1:t, make.HPt)  
+  J <- rep(1, n)
+  I <- diag(n)
+  
+  
+  alpha <- runif(1, alpha.alpha, beta.alpha)
+  mu <- rnorm(1, mu.0, sigma.squared.0)
+  sigma.squared <- 1 / rgamma(1, alpha.sigma.squared, beta.sigma.squared)
+  tau.squared.I <- 1 / rgamma(1, alpha.I, beta.I)
+  tau.squared.P <- 1 / rgamma(1, alpha.P, beta.P)
+  beta.1 <- rnorm(1, mu.beta.1, sigma.squared.beta.1)
+  Ht <- lapply(1:t, make.Ht, beta.1 = beta.1)
+  
+  Q <- riwish(nu.wish, I)
+  Q.inv <- solve(Q)
+  Sigma.epsilon <- sigma.squared * Q
+  Sigma.epsilon.inv <- 1 / sigma.squared * Q.inv
+  Sigma.full <- diag(c(rep(tau.squared.I, NI), rep(tau.squared.P, NP)))
+  Sigma.full.inv <- diag(c(rep(1 / tau.squared.I, NI), rep(1 / tau.squared.P, NP)))
+  Sigma <- vector('list', length = t)
+  Sigma.inv <- vector('list', length = t)
+  for(i in 1:t){
+    Sigma[[i]] <- diag(c(rep(tau.squared.I, NIt[i]), rep(tau.squared.P, NPt[i])))
+    Sigma.inv[[i]] <- diag(c(rep(1 / tau.squared.I, NIt[i]), rep(1 / tau.squared.P, NPt[i])))
+  }  
+  
+  ##
+  ## set up save variables
+  ##
+  
+  X.save <- array(dim = c(t + 1, n, n.mcmc))
+  alpha.save <- vector(length = n.mcmc)
+  mu.save <- vector(length = n.mcmc)
+  phi.save <- vector(length = n.mcmc)
+  tau.I.save <- vector(length = n.mcmc)
+  tau.P.save <- vector(length = n.mcmc)
+  beta.1.save <- vector(length = n.mcmc)
+  sigma.squared.save <- vector(length = n.mcmc)
+  Q.save <- array(dim = c(n, n, n.mcmc))
+  
+  ##
+  ## mcmc loop
+  ##
+  
+  for(l in 1:n.mcmc){
+    if(l %% 100 == 0) cat(' ', l)
+    
+    ##
+    ## sample X
+    ##
+    
+    ## for t = 0
+    A.chol <- chol(alpha^2 * Sigma.epsilon.inv + I / sigma.squared.0.tilde)
+    b <- alpha * Sigma.epsilon.inv %*% (X[2, ] - (1 - alpha) * mu) + mu.0.tilde / sigma.squared.0.tilde
+    X[1, ] <- rMVN(A.chol, b)
+    
+    # for t = 1,...,T - 1
+    for(i in 1:(t - 1)){ # note the offset since t = 0 is the first element of the matrix X
+      A.chol <- chol(t(Ht[[i]]) %*%  Sigma.inv[[i]] %*% Ht[[i]] + (alpha^2 + 1) * Sigma.epsilon.inv)
+      b <- t(Ht[[i]]) %*% Sigma.inv[[i]] %*% (Wt[[i]]) + Sigma.epsilon.inv %*% (alpha * (X[i + 2, ] + X[i, ]) + (1 - alpha)^2 * mu)
+      X[i + 1, ] <- rMVN(A.chol, b)
+    }
+    
+    # for t = T
+    A.chol <- chol(t(Ht[[t]]) %*% Sigma.inv[[t]] %*% Ht[[t]] + Sigma.epsilon.inv)
+    b <- t(Ht[[t]]) %*% Sigma.inv[[t]] %*% (Wt[[t]]) + Sigma.epsilon.inv %*% (alpha * X[t, ] + (1 - alpha) * mu)
+    X[t + 1, ] <- rMVN(A.chol, b)
+    
+    ##
+    ## sample beta_1
+    ##
+    
+    tmp <- vector(length = t)
+    for(i in 1:t){
+      tmp[i] <- t(HPt[[i]] %*% X[i + 1, ]) %*% (HPt[[i]] %*% X[i + 1, ])
+    }
+    A.chol <-sqrt(sum(tmp) / tau.squared.P + 1 / sigma.squared.beta.1)       
+    tmp <- vector(length = t)
+    for(i in 1:t){
+      tmp[i] <- t(HPt[[i]] %*% X[i + 1, ]) %*% (WPt[[i]])
+    }
+    b <- sum(tmp) / tau.squared.P + mu.beta.1 / sigma.squared.beta.1
+    beta.1 <- rMVN(A.chol, b)
+    Ht <- lapply(1:t, make.Ht, beta.1 = beta.1)
+    
+    ##
+    ## sample mu
+    ##
+    
+    #     A.chol <- sqrt((1 - alpha)^2 * (MP / tau.squared.P + MI / tau.squared.I) + 1 / sigma.squared.0)
+    A.chol <- sqrt(t * (1 - alpha)^2 * J %*% Sigma.epsilon.inv %*% J + 1 / sigma.squared.0)
+    tmp <- vector(length = t)
+    for(i in 1:t){
+      tmp[i] <- J %*% Sigma.epsilon.inv %*% (X[i + 1, ] - alpha * X[i, ])
+    }
+    b <- (1 - alpha) * sum(tmp) + mu.0 / sigma.squared.0
+    mu <- rMVN(A.chol, b)
+    
+    ##
+    ## sample alpha
+    ##
+    
+    tmp <- vector(length = t)
+    for(i in 1:t){
+      tmp[i] <- (X[i, ] - mu) %*% Sigma.epsilon.inv %*% (X[i, ] - mu)
+    }
+    A.inv <- 1 / sum(tmp)
+    tmp <- vector(length = t)
+    for(i in 1:t){
+      tmp[i] <- (X[i, ] - mu) %*% Sigma.epsilon.inv %*% (X[i + 1, ] - mu)
+    }
+    b <- sum(tmp)
+    alpha <- rtruncnorm(1, a = 0, b = 1, mean = A.inv * b, sd = sqrt(A.inv))
+    
+    ##
+    ## sample tau.squared.I
+    ##
+    
+    tmp <- vector(length = t)
+    for(i in 1:t){
+      if(NIt[i] != 0){
+        tmp[i] <- (WIt[[i]] - HIt[[i]] %*% X[i + 1, ]) %*% (WIt[[i]] - HIt[[i]] %*% X[i + 1, ])
+      } else {
+        tmp[i] <- 0
+      }
+    }
+    tau.squared.I <- 1 / rgamma(1, alpha.I + MI / 2, beta.I + sum(tmp) / 2)
+    
+    ##
+    ## sample tau.squared.P
+    ##
+    
+    tmp <- vector(length = t)
+    for(i in 1:t){
+
+#       tmp[i] <- t(WPt[[i]] - beta.1 * HPt[[i]] %*% X[i + 1, ] - beta.0) %*% (WPt[[i]] - beta.1 * HPt[[i]] %*% X[i + 1, ] - beta.0)
+      tmp[i] <- t(WPt[[i]] - beta.1 * HPt[[i]] %*% X[i + 1, ]) %*% (WPt[[i]] - beta.1 * HPt[[i]] %*% X[i + 1, ])
+    }
+    tau.squared.P <- 1 / rgamma(1, alpha.P + MP / 2, beta.P + sum(tmp) / 2)
+    
+    for(i in 1:t){
+      Sigma[[i]] <- diag(c(rep(tau.squared.I, NIt[i]),rep(tau.squared.P, NPt[i])))
+      Sigma.inv[[i]] <- diag(c(rep(1 / tau.squared.I, NIt[i]),rep(1 / tau.squared.P, NPt[i])))
+    } 
+    
+    ##
+    ## sample sigma.squared
+    ##
+    
+    tmp <- vector(length = t)
+    for(i in 1:t){
+      tmp[i] <- (X[i + 1, ] - alpha * X[i, ] - (1 - alpha) * mu) %*% Q.inv %*% (X[i + 1, ] - alpha * X[i, ] - (1 - alpha) * mu)
+    }
+    sigma.squared <- 1 / rgamma(1, alpha.sigma.squared + n * t / 2, beta.sigma.squared + sum(tmp) / 2)
+    Sigma.epsilon <- sigma.squared * Q
+    Sigma.epsilon.inv <- 1 / sigma.squared * Q.inv
+    
+    ## 
+    ## sample Q
+    ##
+    
+    
+#     Tbar <- apply(X[ - 1, ], 1, mean) - alpha * apply(X[ - (T + 1), ], 1, mean)
+    Tbar <- apply(X[ - 1, ], 2, mean) - alpha * apply(X[ - (T + 1), ], 2, mean)
+    Tbar.mat <- matrix(rep(Tbar, t), nrow = t, ncol = n, byrow = TRUE)
+    QT <- (X[ - 1, ] -  alpha * X[ - (t + 1), ] - Tbar.mat)
+    Q0 <- t(QT) %*% QT
+    q <- (Tbar - (1 - alpha) * mu)
+    Q <- riwish(t + nu.wish, (Q0 + t * q %*% t(q) + I) / sigma.squared)
+    Q.inv <- solve(Q)
+    Sigma.epsilon. <- sigma.squared * Q
+    Sigma.epsilon.inv <- 1 / sigma.squared * Q.inv
+        
+    ##
+    ## save variables
+    ##
+    
+    X.save[, , l] <- X
+    alpha.save[l] <- alpha
+    mu.save[l] <- mu
+    tau.I.save[l] <- tau.squared.I
+    tau.P.save[l] <- tau.squared.P
+    beta.1.save[l] <- beta.1
+    sigma.squared.save[l] <- sigma.squared
+    Q.save[, , l] <- Q
+  }    
+  
+#   list(X.save = X.save, alpha.save = alpha.save, mu.save = mu.save, phi.save = phi.save, tau.I.save = tau.I.save, tau.P.save = tau.P.save, beta.0.save = beta.0.save, beta.1.save = beta.1.save, sigma.squared.save = sigma.squared.save, rho.save = rho.save, rho.accept = rho.accept)  
+  list(X.save = X.save, alpha.save = alpha.save, mu.save = mu.save, phi.save = phi.save, tau.I.save = tau.I.save, tau.P.save = tau.P.save, beta.1.save = beta.1.save, sigma.squared.save = sigma.squared.save, Q.save = Q.save)  
+}
---
+++ nonSpatialBarcastWishart/script.nonSpatialBarcastWishart.R
@@ -0,0 +1,143 @@
+##
+## This model seems to be working well, maybe reduce the degrees of freedom in the Wishart matrix and run for longer time to get convergence
+##
+
+rm(list = ls())
+
+##
+## libraries and subroutines
+##
+
+library(dplR)
+library(mvtnorm)
+library(MASS)
+library(MCMCpack)
+source('~/barcast/rMVN.R')
+source('~/barcast/plots/make.output.plot.no.beta.0.R')
+source('~/barcast/nonSpatialBarcastWishart/mcmc.nonSpatialBarcastWishart.R')
+
+##
+## load data
+##
+
+pdsi <- read.table('~/barcast/datafiles/Hudson/namerica-pdsi-act.txt', sep = "", header = TRUE)
+Y.crn <- read.crn('~/barcast/datafiles/Hudson/NYCpluvial2010a.allCRNsFinalRmHeaders', header = TRUE)
+# n.trees <- read.table('~/treeRing/datafiles/Hudson/num.trees.txt')
+WP <- Y.crn[, 1:32] - 1
+# Y[idx.na] <- Y[idx.na] - 1
+years <- 1449:2004 # unobserved years include 1449:1899 and 2004
+
+t <- dim(WP)[1]
+# n.tree.mat <- matrix(NA, nrow = t, ncol = n)
+# n.tree.mat[idx.na] <- n.trees$V1
+t.u <- c(1:451, 556) #years PDSI is unobserved
+t.o <- 452:555 # years PDSI is observed
+WI <- rep(NA, length = t)
+WI[t.o] <- pdsi$X261
+
+W <- cbind(WI, WP)
+n <- dim(W)[2]
+H <- !is.na(W)
+H.tmp <- rbind(rep(FALSE, dim(W)[2]), H)
+
+year <- rep(1:t, times = (length(W) / t))
+
+##
+## setup priors
+##
+
+mu.0.tilde <- 0
+sigma.squared.0.tilde <- 1
+alpha.alpha <- 0
+beta.alpha <- 1
+mu.0 <- 0
+sigma.squared.0 <- 1
+alpha.sigma.squared <- 1
+beta.sigma.squared  <- 1
+alpha.phi <- 0.1
+beta.phi <- 0.1
+alpha.I <- 0.1
+beta.I <- 0.1
+alpha.P <- 0.1
+beta.P <- 0.1
+mu.beta.0 <- 0
+sigma.squared.beta.0 <- 8
+mu.beta.1 <- 0
+sigma.squared.beta.1 <- 8
+
+# rho.prior <- 0.75
+# S <- matrix(rho.prior, n, n)
+# diag(S) <- 1
+# S <- solve(S)
+# 
+# R <- matrix(nrow = n,  ncol = n)
+# for(i in 1:n){
+#   for (j in 1:n){
+#     if(i == j){
+#       R[i, j] <- 1
+#     } else{ 
+#       R[i, j] <- rho
+#     }
+#   }
+# }
+n^2 - n - t
+nu.wish <- n + 200 # Must be between n and n^2 - t
+
+
+n.mcmc <- 5000
+n.burn <- floor(n.mcmc / 5)
+
+##
+## fit MCMC
+##
+
+start <- Sys.time()
+# Rprof(file = '~/barcast/nonSpatialBarcast/Rprof.out', lines = 'show')
+out <- mcmc(WI, WP, n.mcmc, mu.0.tilde, sigma.squared.0.tilde, alpha.alpha, beta.alpha, mu.0, sigma.squared.0, alpha.sigma.squared, beta.sigma.squared, alpha.phi, beta.phi, alpha.I, beta.I, alpha.P, beta.P, mu.beta.0, sigma.squared.beta.0, mu.beta.1, sigma.squared.beta.1, nu.wish)
+# Rprof(NULL)
+# summaryRprof('~/barcast/nonSpatialBarcast/Rprof.out')
+finish <- Sys.time() - start
+finish 
+
+# x11()
+make.output.plot(out)
+# make.output.plot(out, file = '~/barcast/plots/wishartCorrelation3.31.2014.jpg')
+# make.output.plot(out, model = 'simple', file = '~/treeRing/plots/simple.log.climate.png')
+# 
+# for(i in 1:32){
+#   matplot(apply(out$X.save[, i, n.burn:n.mcmc], 1, mean), type = 'l')
+# }
+# 
+# ##
+# ## Plot using the longest tree ring reconstruction
+# ##
+# 
+# layout(1:2)
+# matplot(apply(out$X.save[, 13, n.burn:n.mcmc], 1, mean), type = 'l')
+# abline(h = 0, col = 'blue')
+# lines(apply(out$X.save[, 13, n.burn:n.mcmc], 1, quantile, probs = 0.025), col = adjustcolor('red', alpha = 0.25))
+# lines(apply(out$X.save[, 13, n.burn:n.mcmc], 1, quantile, probs = 0.975), col = adjustcolor('red', alpha = 0.25))
+# lines(WI, col = adjustcolor('blue', alpha = 0.5))
+# y <- apply(out$X.save[, 13, n.burn:n.mcmc], 1, mean)
+# x <- 1:(t+1)
+# abline(lm(y~x), col = 'green')
+# # 			plot(X, type = 'l', col = 'blue')
+# plot(sqrt((WI[t.o] - apply(out$X.save[, 13, n.burn:n.mcmc], 1, mean)[t.o + 1])^2), type = 'l', main = 'RMSE for PDSI', ylab = 'RMSE for PDSI')
+# 
+
+X.mat <- matrix(NA, nrow = t + 1, dim(H.tmp)[2])
+X.save <- matrix(0, nrow = t + 1, n.mcmc - n.burn)
+for(k in (n.burn + 1):n.mcmc){
+  if(k %% 100 == 0){
+    cat(k, ' ')
+  }
+  X.mat[H.tmp] <- out$X.save[, , k][H.tmp]
+  X.save[, k - n.burn] <- rowMeans(X.mat, na.rm = TRUE)
+}
+
+matplot(apply(X.save[-1, ], 1, mean), type = 'l')
+abline(h=0, col = 'red')
+lines(apply(X.save[-1, ], 1, quantile, prob = 0.025), col = adjustcolor('red', alpha = 0.25))
+lines(apply(X.save[-1, ], 1, quantile, prob = 0.975), col = adjustcolor('red', alpha = 0.25))
+lines(WI, col = adjustcolor('blue', alpha = 0.5))
+
